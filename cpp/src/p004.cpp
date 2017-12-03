677c815b3aee8f81fede1813272f727f40a75518
Haralambi Todorov <harrytodorov@gmail.com>
2017-12-03 22:19:36 +0100

    - problem 4

---
+++ cpp/src/p004.cpp
@@ -0,0 +1,51 @@
+//
+// Created by Haralambi Todorov on 17/08/2017.
+//
+
+/// Problem 4:
+/// A palindromic number reads the same both ways. The largest palindrome made from the product
+/// of two 2-digit numbers is 9009 = 91 × 99.
+/// Find the largest palindrome made from the product of two 3-digit numbers.
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+bool isPalindrome(const int n) {
+  // Could not be a palindrome.
+  // One does not consider negative numbers.
+  if (n < 10)
+    return false;
+  int numberOfDigitis = static_cast<int>(log10(n) + 1);
+  int nCopy = n;
+  int multiple = static_cast<int>(pow(10, numberOfDigitis / 2 - 1));
+  int palindrome = (nCopy % 10) * multiple;
+  nCopy /= 10;
+  while (nCopy >= palindrome && multiple > 0) {
+    if (nCopy == palindrome)
+      return true;
+    multiple /= 10;
+    palindrome += (nCopy % 10) * multiple;
+    nCopy /= 10;
+  }
+  return false;
+}
+
+int main() {
+  std::vector<std::pair<bool, bool>> digits;
+  digits.reserve(998001);
+  std::fill(digits.begin(), digits.end(), std::make_pair(false, false));
+
+  size_t max = 0;
+  for (size_t i = 100; i < 1000; i++) {
+    for (size_t j = 100; j < 1000; j++) {
+      if (!digits[i*j].first) {
+        digits[i*j].first = true;
+        digits[i*j].second = isPalindrome(i*j);
+        if (digits[i*j].second && max < i*j) max = i*j;
+      }
+    }
+  }
+  std::cout << "Largest palindrome made from product of 3-digit numbers: " <<
+            max << std::endl;
+}
