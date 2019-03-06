2b2794fd9f3caa50833f40b9fe69efe52688eccb
Haralambi Todorov <haralambi.todorov@iwm.fraunhofer.de>
2019-03-06 13:05:22 +0100

    p001: Simplify problem solution

--- cpp/src/p001.cpp
+++ cpp/src/p001.cpp
@@ -6,28 +6,17 @@
 /// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 /// Find the sum of all the multiples of 3 or 5 below 1000.
 
-#include <cmath>
+#include <iostream>
 #include <numeric>
 #include <set>
 
 void multiples_of_3_and_5_below_n(const int n, std::set<int> &ra) {
-    int i = 1;
-    int p3, p5;
-    while (i*3 < n) {
-        // calculate 3 & 5 to the power i
-        p3 = 3*i;
-        p5 = 5*i;
-
-        // because set only can contain unique elements, we don't have to double check
-        // if the element we're inserting is already contained in the resulting set
-        ra.insert(p3);
-
-        // add p5 to the resulting set only if its less than n
-        if (p5 < n)
-            ra.insert(p5);
-
-        // increment i
-        i++;
+    // Iterate over the n numbers
+    for (int i = 1; i < n; i++) {
+        // Check if a number is a multiple of 3/5
+        if ((i % 3 == 0) || (i % 5 == 0)) {
+            ra.insert(i);
+        }
     }
 }
 
@@ -37,7 +26,7 @@ int main() {
     // test if the function works for the given test
     multiples_of_3_and_5_below_n(10, res);
     int res10 = std::accumulate(res.begin(), res.end(), (int) 0);
-    printf("Sum of multiples of 3 and 5 below 10: %d\n", res10);
+    std::cout << "Sum of multiples of 3 and 5 below 10: " << res10 << std::endl;
 
     // clear the set holding the result
     res.clear();
@@ -45,5 +34,5 @@ int main() {
     // test for input of 1000
     multiples_of_3_and_5_below_n(1000, res);
     int res1000 = std::accumulate(res.begin(), res.end(), (int) 0);
-    printf("Sum of multiples of 3 and 5 below 1000: %d\n", res1000);
+    std::cout << "Sum of multiples of 3 and 5 below 10: " << res1000 << std::endl;
 }
