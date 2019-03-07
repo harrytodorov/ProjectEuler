4d0d207320c7b90bd3ad79233669661590723f33
Haralambi Todorov <haralambi.todorov@iwm.fraunhofer.de>
2019-03-07 15:46:35 +0100

    Created a cool function to automize the execution of different problems; Adapted the execution of problems 1 and 2.

--- cpp/src/ProjectEulerMain.cpp
+++ cpp/src/ProjectEulerMain.cpp
@@ -6,14 +6,28 @@
 
 // Problems
 #include "problems/p001.cpp"
+#include "problems/p002.cpp"
+
+void execute_problem(int (*problem)(int),
+					 const int problem_input,
+					 const int problem_number) {
+	auto s001 = std::chrono::system_clock::now();
+	auto p001_result = (*problem)(problem_input);
+	auto e001 = std::chrono::system_clock::now();
+	std::cout << "Result of problem " << problem_number
+			  << ": " << p001_result << std::endl;
+	auto ellapsed_milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(e001-s001);
+	std::cout << "Solved in " << ellapsed_milliseconds.count()
+			  << " milliseconds." << std::endl;
+	std::cout << "---" << std::endl;
+}
 
 // Main driver for solving Project Euler problems
 int main(int argc, char** argv) {
 	// Problem 1
-	auto s001 = std::chrono::system_clock::now();
-	auto p001_result = problem001(1000);
-	auto e001 = std::chrono::system_clock::now();
-	std::cout << "Result of problem 001: " << p001_result << std::endl;
-	auto ellapsed_milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(e001-s001);
-	std::cout << "Solved in " << ellapsed_milliseconds.count() << " milliseconds." << std::endl;
+	execute_problem(problem001, 1000, 1);
+
+	// Probmlem 2
+	execute_problem(problem002, 4000000, 2);
+
 }
--- cpp/src/problems/p002.cpp
+++ cpp/src/problems/p002.cpp
@@ -3,30 +3,19 @@
 
 #include <iostream>
 #include <cassert>
-#include "utilities.h"
+#include "../utilities.h"
 
-int main() {
-
-    // A small test case
-    // Find the sum of the even valued terms of the first 10 Fibonacci numbers
-    // starting with 1 and 2
-    // {2, 8, 34} = 44
+/**
+ * Find the sum of the even-valued terms in the Fibonacci sequence whose
+ * values do not exceed n.
+ * The Fibonacci sequence starts with terms 1 and 2.
+ */
+int problem002(int n) {
     std::vector<int> fibs;
     int sum = 0;
-    generate_fibonacci_numbers(10, 1, 2, fibs);
+    generate_fibonacci_less_than(n, 1, 2, fibs);
     for (const auto& fib : fibs) {
         if ((fib % 2) == 0) sum += fib;
     }
-    assert(sum == 44);
-
-    // Problem case
-    sum = 0;
-    fibs.clear();
-    generate_fibonacci_less_than(4000000, 1, 2, fibs);
-    for (const auto& fib : fibs) {
-        if ((fib % 2) == 0) sum += fib;
-    }
-
-    std::cout << "Sum of fibonacci numbers which do not exceed 4,000,000 "
-              << "and are even is: " <<  sum << std::endl;
+    return sum;
 }
